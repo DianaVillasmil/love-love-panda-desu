2ffaf275b6d140ee616d59b1bf57c8667f10561a
Diana Villasmil <diacavipi@gmail.com>
2017-05-21 19:10:26 -0400

    love love panda

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+var pictures = ["assets/img/a1.jpg",
+				"assets/img/a2.jpg", 
+				"assets/img/a3.jpg", 
+				"assets/img/a4.jpg"];
+var imagenes = document.getElementById("imagenes");
+pictures.forEach(function(url) {
+	var li = document.createElement("li");
+	var img = document.createElement("img");
+	li.appendChild(img);
+	img.setAttribute("src", url);
+	imagenes.appendChild(li);
+})
